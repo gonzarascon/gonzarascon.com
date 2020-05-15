a4e70fa5c6ded4820839afaab9ee83fe9b3765ce
gonzarascon <gonzarascon@gmail.com>
2020-05-15 12:07:21 -0300

    Sitemap.xml test

--- pages/api/sitemap.xml.js
+++ pages/api/sitemap.xml.js
@@ -4,11 +4,20 @@ import { SitemapStream, streamToPromise, EnumChangefreq } from 'sitemap';
 import { createGzip } from 'zlib';
 
 export default async (req, res) => {
+  let sitemap = null;
   if (!res) return {};
   try {
     // Set response header
     res.setHeader('content-type', 'application/xml');
     res.setHeader('Content-Encoding', 'gzip');
+
+    if (sitemap) {
+      res.write(sitemap);
+      res.end();
+      return {
+        props: {},
+      };
+    }
 
     // A Transform for turning a Readable stream of either SitemapItemOptions or url strings into a Sitemap.
     // The readable stream it transforms must be in object mode.
@@ -20,18 +29,17 @@ export default async (req, res) => {
     // Add any static entries here
     smStream.write({
       url: '/',
-      lastmod: process.env.siteUpdatedAt,
       changefreq: EnumChangefreq.DAILY,
     });
     smStream.end();
 
     // cache the response
     // streamToPromise.then(sm => sitemap = sm)
-    streamToPromise(pipeline);
+    const resp = await streamToPromise(pipeline);
     // stream the response
-    pipeline.pipe(res).on('error', e => {
-      throw e;
-    });
+
+    res.write(resp);
+    res.end();
   } catch (e) {
     res.status(500).end();
   }
