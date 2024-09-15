2ef92e3d8c239a06be21086bf568c5734ba29e97
gonzarascon <gonzarascon@gmail.com>
2024-09-15 13:02:43 -0300

    feat:drawy + biome config

--- app/entry.client.tsx
+++ app/entry.client.tsx
@@ -5,14 +5,27 @@
  */
 
 import { RemixBrowser } from "@remix-run/react";
-import { startTransition, StrictMode } from "react";
+import { startTransition, StrictMode, useEffect } from "react";
 import { hydrateRoot } from "react-dom/client";
+import posthog from "posthog-js";
+
+function PosthogInit() {
+	useEffect(() => {
+		posthog.init("phc_OLZzWZy2gthIhMddcgsDubT8AFfaaLvd5WeOTeIRBZ8", {
+			api_host: "https://us.i.posthog.com",
+			person_profiles: "always", // or 'always' to create profiles for anonymous users as well
+		});
+	}, []);
+
+	return null;
+}
 
 startTransition(() => {
-  hydrateRoot(
-    document,
-    <StrictMode>
-      <RemixBrowser />
-    </StrictMode>
-  );
+	hydrateRoot(
+		document,
+		<StrictMode>
+			<RemixBrowser />
+			<PosthogInit />
+		</StrictMode>,
+	);
 });
--- app/root.tsx
+++ app/root.tsx
@@ -1,57 +1,42 @@
-import { cssBundleHref } from '@remix-run/css-bundle';
-import { json, type LinksFunction } from '@remix-run/node';
+import { type LinksFunction } from "@remix-run/node";
 import {
-  Links,
-  LiveReload,
-  Meta,
-  Outlet,
-  Scripts,
-  ScrollRestoration,
-  useLoaderData,
-} from '@remix-run/react';
+	Links,
+	Meta,
+	Outlet,
+	Scripts,
+	ScrollRestoration,
+} from "@remix-run/react";
 
-import styles from './styles.css';
-import { getPublicEnvs } from './environment.server';
-import { PublicEnv } from './components/PublicEnv';
-import { AnalyticsScript } from './components/AnalyticsScript';
+import styles from "./tailwind.css?url";
 
 export const links: LinksFunction = () => [
-  { rel: 'preconnect', href: 'https://fonts.googleapis.com' },
-  {
-    rel: 'preconnect',
-    href: 'https://fonts.gstatic.com',
-    crossOrigin: 'anonymous',
-  },
-  ...(cssBundleHref ? [{ rel: 'stylesheet', href: cssBundleHref }] : []),
-  { rel: 'stylesheet', href: styles },
-  {
-    rel: 'stylesheet',
-    href: 'https://fonts.googleapis.com/css2?family=Work+Sans:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap',
-  },
+	{ rel: "preconnect", href: "https://fonts.googleapis.com" },
+	{
+		rel: "preconnect",
+		href: "https://fonts.gstatic.com",
+		crossOrigin: "anonymous",
+	},
+	{ rel: "stylesheet", href: styles },
+	{
+		rel: "stylesheet",
+		href: "https://fonts.googleapis.com/css2?family=Work+Sans:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap",
+	},
 ];
 
-export async function loader() {
-  return json(getPublicEnvs());
-}
-
 export default function App() {
-  const { ENV } = useLoaderData<typeof loader>();
-  return (
-    <html lang="en">
-      <head>
-        <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width,initial-scale=1" />
-        <Meta />
-        <Links />
-      </head>
-      <body className="bg-slate-50">
-        <AnalyticsScript />
-        <Outlet />
-        <ScrollRestoration />
-        <PublicEnv {...ENV} />
-        <Scripts />
-        <LiveReload />
-      </body>
-    </html>
-  );
+	return (
+		<html lang="en">
+			<head>
+				<meta charSet="utf-8" />
+				<meta name="viewport" content="width=device-width,initial-scale=1" />
+				<Meta />
+				<Links />
+			</head>
+			<body className="bg-slate-50">
+				<Outlet />
+				<ScrollRestoration />
+				<Scripts />
+			</body>
+		</html>
+	);
 }
