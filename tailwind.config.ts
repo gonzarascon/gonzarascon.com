3334e293ad872cd97a3ad7bc4b3ee1ae3da7f5e3
gonzarascon <gonzarascon@gmail.com>
2024-09-17 21:48:09 -0300

    updated drawy tw config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -10,6 +10,21 @@ export default {
 			fontFamily: {
 				serif: ["Playfair Display", "ui-serif", "Georgia", "serif"],
 			},
+			keyframes: {
+				"slide-in-right": {
+					from: { transform: "translate3d(100%,0,0)" },
+					to: { transform: "translate3d(0,0,0)" },
+				},
+				"slide-out-right": {
+					from: { transform: "translate3d(0,0,0)" },
+					to: { transform: "translate3d(100%,0,0)" },
+				},
+			},
+			animation: {
+				"slide-out-right":
+					"slide-out-right 150ms cubic-bezier(0.22, 1, 0.36, 1)",
+				"slide-in-right": "slide-in-right 150ms cubic-bezier(0.22, 1, 0.36, 1)",
+			},
 		},
 	},
 	plugins: [],
