73fa7054521b05787101ee668ebcc1bb0e8e2560
gonzarascon <gonzarascon@gmail.com>
2024-09-17 23:27:01 -0300

    updated tailwind content

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,7 +3,7 @@ import type { Config } from "tailwindcss";
 export default {
 	content: [
 		"./app/**/{**,.client,.server}/**/*.{js,jsx,ts,tsx}",
-		"./node_modules/drawy/src/**/*.{js,jsx,ts,tsx}",
+		"./node_modules/drawy/**/*.{js,jsx,ts,tsx}",
 	],
 	theme: {
 		extend: {
